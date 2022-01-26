eaa4c9fa4bace38b06188241a4bcd74ab94a575c
Obrad Mrdalj <omrdalj88@yahoo.com>
2022-01-26 18:12:15 +0100

    Products section finished.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let slideIndex = 1;
+// Carousel Functionality
+const slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -22,3 +23,78 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
 }
+// end
+
+// Cars cards
+const carsContainer = document.querySelector('.cars');
+const cars = [
+    {
+        id: 1,
+        info: '5 Seats, 4 Doors',
+        name: 'Audi Q3 2020',
+        image: './images/audi-q3.png',
+        specs: ['Includes 300 free km', 'Theft Protection', 'Non Refundable'],
+        price: '29.999 €'
+    },
+    {
+        id: 2,
+        info: '5 Seats, 4 Doors',
+        name: 'Audi A4 2020',
+        image: './images/audi-a4.png',
+        specs: ['Includes 300 free km', 'Theft Protection', 'Non Refundable'],
+        price: '24.999 €'
+    },
+    {
+        id: 3,
+        info: '5 Seats, 4 Doors',
+        name: 'Audi A3 2020',
+        image: './images/audi-a3.png',
+        specs: ['Includes 300 free km', 'Theft Protection', 'Non Refundable'],
+        price: '19.999 €'
+    },
+    {
+        id: 4,
+        info: '5 Seats, 4 Doors',
+        name: 'Audi Q7 2020',
+        image: './images/audi-q7.png',
+        specs: ['Includes 300 free km', 'Theft Protection', 'Non Refundable'],
+        price: '39.999 €'
+    }
+];
+
+function displayCars() {
+    cars.forEach(car => {
+        const carCard = document.createElement('div');
+        carCard.classList.add('car');
+        carCard.innerHTML = `
+        <img class="car-img" src="${car.image}" alt="">
+        <p class="car-info">${car.info}</p>
+        <p class="car-name">${car.name}</p>
+        <a href="#" class="btn">Configurator</a>
+        <ul class="specs">
+            <li class="spec">
+                <span class="checkmark">&#10003;</span>
+                <span>${car.specs[0]}</span>
+            </li>
+            <li class="spec">
+                <span class="checkmark">&#10003;</span>
+                <span>${car.specs[1]}</span>
+            </li>
+            <li class="spec">
+                <span class="checkmark">&#10003;</span>
+                <span>${car.specs[2]}</span>
+            </li>
+        </ul>
+        <div class="bottom-selection">
+            <div class="price">
+                <label>Starting from</label>
+                <p class="price-amount">${car.price}</p>
+            </div>
+            <button>Buy Now</button>
+        </div>
+        `;
+        carsContainer.appendChild(carCard);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', displayCars())
